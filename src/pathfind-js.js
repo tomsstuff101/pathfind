cedbe7f7da8420ca5243fafa54a31622143d6c64
Tom Ormiston <tom@appijumbo.com>
2022-04-12 20:45:03 +0100

    find valid working

--- src/pathfind-js.js
+++ src/pathfind-js.js
@@ -10,16 +10,115 @@ const pathfind = (A, P, Q) => {
   /* Init   */
   /***********/
 
-  let cellGHFArray = Array.from(Array(A.length), () => new Array(A.length)); // create cellArray for storing G, H, F and parent
+  //let cellGHFArray = Array.from(Array(A.length), () => new Array(A.length)); // create arrayGHF for storing G, H, F and parent
+
+  let arrayGHF = [];
+
+  // square array
+  for (let index = 0; index < A.length; index++) {
+    let row = [];
+    for (let rowIndex = 0; rowIndex < A.length; rowIndex++) {
+      row.push({ G: -1, H: -1, F: -1 }); // IF hedge or path not found then same ie all -1 around Q
+    }
+    arrayGHF.push(row);
+  }
+
+  //   console.log("arrayGHF");
+  //   console.table(arrayGHF);
+
   let openList = []; // cells being considered
   let closedList = []; // GH and F calculated
   let nextList = []; // the next cells to be considered
 
   //let validState = true;
 
+  const initOpenList = (cell) => {
+    let north = { x: cell.x, y: cell.y - 1 };
+    let east = { x: cell.x + 1, y: cell.y };
+    let south = { x: cell.x, y: cell.y + 1 };
+    let west = { x: cell.x - 1, y: cell.y };
+
+    let initOpenDirections = [north, east, south, west];
+
+    // console.log("init open list directions --> ");
+    // console.log(initOpenDirections);
+
+    initOpenDirections.forEach((direction) => {
+      //   console.log(`A direction -->  x ${direction.x}   y ${direction.y} `);
+
+      if (direction.x >= 0 && direction.x < A.length) {
+        if (direction.y >= 0 && direction.y < A.length) {
+          if (A[direction.y][direction.x] === true) {
+            // its not a wall
+
+            openList.push(direction);
+          }
+        }
+      }
+    });
+
+    // console.log("Open List -->");
+    // console.log(openList);
+  };
+
   const findNextCells = () => {
     // Find list of valid vectors for nextList
     // open cells pushes to next list.
+    //
+    //
+    // test each openList vector N,E,S and W (no diagonals)
+    // ignore if wall or if out of bounds
+    // otherwise add its vector to the nextList
+
+    // console.log("openList at findNextCells == ");
+    // console.log(openList);
+
+    openList.forEach((cell) => {
+      let north = { x: cell.x, y: cell.y - 1 };
+      let east = { x: cell.x + 1, y: cell.y };
+      let south = { x: cell.x, y: cell.y + 1 };
+      let west = { x: cell.x - 1, y: cell.y };
+
+      let checkList = [north, east, south, west];
+
+      console.log("find next cells for");
+      console.log(cell);
+      console.log(" --  check list directions --> ");
+      console.log(checkList);
+
+      checkList.forEach((direction) => {
+        // its not off the map
+        console.log("considering adding --> ", direction);
+        if (direction.x >= 0 && direction.x < A.length) {
+          if (direction.y >= 0 && direction.y < A.length) {
+            // its not a wall
+            console.log(
+              `direction x ${direction.x}    direction y ${direction.y}`
+            );
+
+            if (A[direction.y][direction.x] === true) {
+              console.log(
+                `not a wall direction x ${direction.x}    direction y ${direction.y}`
+              );
+
+              // see if in the closed list
+              console.log(closedList);
+              let inClosedList = closedList.filter((cell) => {
+                return cell.x === direction.x && cell.y === direction.y;
+              });
+
+              console.log("inClosed list -- ", inClosedList);
+
+              //   if (closedList.indexOf(direction) === -1) {
+              if (inClosedList.length === 0) {
+                console.log("ADDING ==> ", direction);
+                nextList.push(direction);
+              }
+            }
+          }
+        }
+      });
+    });
   };
 
   const findGHFforAllOpenCells = () => {
@@ -42,6 +141,10 @@ const pathfind = (A, P, Q) => {
    * Initialse the search
    * * */
   closedList.push(P);
+  initOpenList(P);
+
+  //   console.log("open List at init");
+  //   console.log(openList);
 
   //   while (qNotFound() && validSate) {
 
@@ -50,26 +153,38 @@ const pathfind = (A, P, Q) => {
     do {
       //Calculate G, H & F for all valid cells starting at P,
       // working outwards untill Q is found or its impossible ie no path
+      //   findGHFforAllOpenCells();
+      nextList = [];
       findNextCells();
       closedList = [...closedList, ...openList];
+      console.log("closedList----");
+      console.log(closedList);
       openList = [...nextList];
-      findGHFforAllOpenCells();
-    } while (openList !== []);
+
+      console.log("openList----");
+      console.log(openList);
+      console.log("nextList---");
+      console.log(nextList);
+    } while (openList.length !== 0);
 
     //find minimum steps
     const stepsToQ = () => {
       // find lowest G for each valid cell in cellGHFArray for position Q
-
+      lowestG = 88;
+      console.log("lowest G ", lowestG);
       return lowestG;
     };
+
+    return stepsToQ();
   } else {
     console.log("P and Q same");
     return 0;
   }
 
-  return -1;
+  //return -1;
 };
-
+// [true, false, false, false, false],
+//[true, true, true, true, true],
 const A = [
   [true, true, true, true, true],
   [true, false, false, false, true],
@@ -77,7 +192,7 @@ const A = [
   [true, true, true, true, true],
   [true, true, true, true, true],
 ];
-const P = { x: 0, y: 0 };
-const Q = { x: 0, y: 0 };
+const P = { x: 1, y: 0 };
+const Q = { x: 3, y: 3 };
 
 pathfind(A, P, Q);
